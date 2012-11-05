f6e024b027108692003265db08010f0c5f5a7ba3
George V. Reilly <george@reilly.org>
2012-11-04 18:00:57 -0800

    config with clang

--- config.sh
+++ config.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 cd src
-./configure \
+CC=clang ./configure \
     --with-features=huge \
     --enable-perlinterp \
     --enable-pythoninterp \
-    --enable-tclinterp --with-tcl=tclsh8.4 \
     --enable-rubyinterp \
-    --disable-mzschemeinterp \
-    --with-plthome=/usr/local/plt \
     --enable-cscope \
     --enable-multibyte \
     --with-compiledby=george@reilly.org \
 
+#   --enable-tclinterp --with-tcl=tclsh8.4 \
+#   --disable-mzschemeinterp \
+#   --with-plthome=/usr/local/plt \
 #   --enable-gnome-check \
